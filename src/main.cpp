66a2dc925d1d371b79b8f359c85a3838c3567a4c
jamiekang7 <jamiekang0@gmail.com>
2026-01-24 16:19:04 -0800

    working motor open loop test

--- src/main.cpp
+++ src/main.cpp
@@ -15,22 +15,32 @@ BLDCMotor motor = BLDCMotor(4); // set up for 4 pole pairs
 // BLDCDriver6PWM( int phA_h, int phA_l, int phB_h, int phB_l, int phC_h, int phC_l, int en)
 BLDCDriver6PWM driver = BLDCDriver6PWM(PIN_UH, PIN_UL, PIN_VH, PIN_VL, PIN_WH, PIN_WL, PIN_EN); 
 
+//instantiate the commander
+Commander command = Commander(Serial);
+void doTarget(char* cmd) { command.scalar(&motor.target, cmd); }
+void doLimitCurrent(char* cmd) { command.scalar(&motor.current_limit, cmd); }
+
 // Max Voltage: 6-8V (7.4V nominasl, use 6V for safety)
 // 5-10% PWM Duty Cycle (to be safe)
 
 void setup() {
- 
+  
   // PWM frequency
-  driver.pwm_frequency = 25000;
+  driver.pwm_frequency = 30000;
 
   // dead_zone [0,1] - default 0.02 - 2%
   driver.dead_zone = 0.05; // default set up
-  driver.dead_time = 1/driver.pwm_frequency * driver.dead_zone;
+  // dead_time = 1/driver.pwm_frequency*driver.dead_zone;
 
   // Driver Voltages
   driver.voltage_limit = 5;
   driver.voltage_power_supply = 3.3;
    
+  // limiting voltage 
+  motor.voltage_limit = 3;   // Volts
+  // or current  - if phase resistance provided
+  motor.current_limit = 0.5; // Amps
+
   //linking motor to driver
 
   Serial.print("Driver init ");
@@ -41,9 +51,14 @@ void setup() {
     return;
 }
     motor.linkDriver(&driver);
-    motor.controller = MotionControlType::voltage;
+   motor.controller = MotionControlType::velocity_openloop;
     
-    motor.init()
+  motor.init();
+  motor.initFOC();
+
+
+  command.add('T', doTarget, "target velocity");
+  command.add('C', doLimitCurrent, "current limit");
   _delay(1000);
 
 
@@ -55,6 +70,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   
   //FOC algorithm execution
-  
+  motor.loopFOC();
   motor.move(target_voltage);
+  command.run();
 }
